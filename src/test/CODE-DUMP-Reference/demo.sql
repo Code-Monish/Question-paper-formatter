4f760aa3be57010da32cd6838ac3785d9931fd0b
Nihaarika321 <nihaarika64@gmail.com>
2024-11-07 20:57:15 +0530

    a

--- src/test/CODE-DUMP-Reference/demo.sql
+++ src/test/CODE-DUMP-Reference/demo.sql
@@ -1,4 +1,4 @@
-CREATE TABLE employee (empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, salary NOT NULL);
+CREATE TABLE employee(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, salary NOT NULL);
 INSERT INTO employee(empid, empname, salary) VALUES(1, 'JAY', 12000);
 INSERT INTO employee(empid, empname, salary) VALUES(2, 'KAVIN', 20000);
 
